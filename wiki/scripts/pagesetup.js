2512bce55f3ab7017c27f12099ec3efed2531e9b
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2020-12-04 18:15:01 -0500

    Update pagesetup.js

--- wiki/scripts/pagesetup.js
+++ wiki/scripts/pagesetup.js
@@ -1,3 +1,55 @@
+function pageTemplate(page) {
+	document.write(`
+		<h1 style="font-family: Ubuntu;">${page.name}</h1>
+		
+		<hr>
+	`);
+	if (page.navbox !== undefined) {
+		document.write(`
+		<table class="infobox" style="width:22em">
+			<caption>${page.name}</caption>
+			<tbody>
+				<tr>
+					<td colspan="2" style="text-align:center">
+						<img alt="${page.file}" src="./images/${page.file}" width="330" height="330">
+						<div>
+							<a>${page.caption}</a>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th colspan="2" style="">${page.heading}</th>
+				</tr>
+		`);
+		for (let i = 0; i < page.navbox.length; i++) {
+			document.write(`
+				<tr>
+					<th scope="row" style="max-width:11em;">
+						<div style="display:inline-block; padding:0.1em 0;line-height:1.2em;">
+							${page.navbox[i].name}
+						</div>
+					</th>
+					<td>
+						<div class="plainlist">
+							<ul>
+								<li>
+									<span class="nowrap">
+										${page.navbox[i].info.toString().replaceAll(",","</br>")}
+									</span>
+								</li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+			`);
+		}
+		document.write(`
+			</tbody>
+		</table>
+		`);
+	}
+}
+
 function generatePage(page) {
 	switch (page) {
 		case "main":
@@ -12,161 +64,265 @@ function generatePage(page) {
 			<p>
 				<a href="index.html?p=aero" class="n">Aero</a></br>
 				<a href="index.html?p=archie" class="n">Archie</a></br>
-				<a href="index.html?p=lacus" class="n">Lacus</a></br>
+				<a href="index.html?p=lakys" class="n">Lakys</a></br>
 				<a href="index.html?p=aposteel" class="n">Aposteel</a></br>
 			</p>
 			
 			<h2>Planets</h2>
 			<p>
-				<a href="index.html?p=grene" class="n">Grene</a></br>
-				<a href="index.html?p=doug" class="n">Doug</a></br>
+				<a href="index.html?p=grene" class="y">Grene</a></br>
+				<a href="index.html?p=doug" class="y">Doug</a></br>
 				<a href="index.html?p=pilf" class="y">Pilf</a></br>
-				<a href="index.html?p=ert" class="n">Ert</a></br>
-				<a href="index.html?p=george" class="n">George</a></br>
-				<a href="index.html?p=dough" class="n">Dough</a></br>
-				<a href="index.html?p=mac" class="n">Mac</a></br>
-				<a href="index.html?p=aeiou" class="n">Aeiou</a></br>
-				<a href="index.html?p=the" class="n">The</a></br>
-				<a href="index.html?p=cea" class="n">Cea</a></br>
+				<a href="index.html?p=ert" class="y">Ert</a></br>
+				<a href="index.html?p=george" class="y">George</a></br>
+				<a href="index.html?p=dough" class="y">Dough</a></br>
+				<a href="index.html?p=mac" class="y">Mac</a></br>
+				<a href="index.html?p=wop" class="y">World of Pain</a></br>
+				<a href="index.html?p=aeiou" class="y">Aeiou</a></br>
+				<a href="index.html?p=the" class="y">The</a></br>
+				<a href="index.html?p=cea" class="y">Cea</a></br>
 			</p>
 			
 			</br>
 			</br>
 			`);
 			break;
+		case "grene":
+			pageTemplate({
+				name: 'Grenē',
+				file: 'grene.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['None']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Chthonian']
+				}, {
+					name: 'Life',
+					info: ['None']
+				}]
+			});
+			break;
+		case "doug":
+			pageTemplate({
+				name: 'Doug',
+				file: 'doug.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['H']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Terrestrial']
+				}, {
+					name: 'Life',
+					info: ['Worms (Unconfirmed)']
+				}]
+			});
+			break;
 		case "pilf":
-			document.write(`
-			<h1 style="font-family: Ubuntu;">Pilf</h1>
-			
-			<hr>
-			
-			<table class="infobox" style="width:22em">
-				<caption>Pilf</caption>
-				<tbody>
-					<tr>
-						<td colspan="2" style="text-align:center">
-							<img alt="pilf.jpg" src="./images/pilf.png" width="330" height="330">
-							<div>
-								<a>Pictured in natural color, illustration made by Gramiatar.</a>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<th colspan="2" style="">Designations</th>
-					</tr>
-					<tr>
-						<th scope="row" style="max-width:11em;">
-							<div style="display:inline-block; padding:0.1em 0;line-height:1.2em;">
-								Known Sattelites
-							</div>
-						</th>
-						<td>
-							<div class="plainlist">
-								<ul>
-									<li>
-										<span class="nowrap">
-											Piffol Rings
-										</span>
-									</li>
-								</ul>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row" style="max-width:11em;">
-							<div style="display:inline-block; padding:0.1em 0;line-height:1.2em;">
-								Named origin
-							</div>
-						</th>
-						<td>
-							<div class="plainlist">
-								<ul>
-									<li>
-										<span class="nowrap">
-											Unknown (See <a href="index.html?p=ert" class="n">Flip</a>)
-										</span>
-									</li>
-								</ul>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row" style="max-width:11em;">
-							<div style="display:inline-block; padding:0.1em 0;line-height:1.2em;">
-								Classification
-							</div>
-						</th>
-						<td>
-							<div class="plainlist">
-								<ul>
-									<li>
-										<span class="nowrap">
-											Cuboid Terrestrial Planet
-										</span>
-									</li>
-								</ul>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row" style="max-width:11em;">
-							<div style="display:inline-block; padding:0.1em 0;line-height:1.2em;">
-								Life
-							</div>
-						</th>
-						<td>
-							<div class="plainlist">
-								<ul>
-									<li>
-										<span class="nowrap">
-											Cheese Flies
-										</span>
-									</li>
-								</ul>
-							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			
-
-			<p>Pilf is the fourth planet from the <a href="index.html?p=cold_sun" class="n">Hot Sun</a>. It is the seventh largest in size, after <a href="index.html?p=ert" class="n">Ert</a> and before <a href="index.html?p=doug" class="n">Doug</a>, and is also the seventh most massive planet, after <a href="index.html?p=ert" class="n">Ert</a> and before <a href="index.html?p=george" class="n">George</a>.</p>
-
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-				</br>
-			`);
+			pageTemplate({
+				name: 'Pilf',
+				file: 'pilf.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['Piffol Rings']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown (See <a href="index.html?p=flip" class="n">Flip</a>)']
+				}, {
+					name: 'Classification',
+					info: ['Cuboid Terrestrial']
+				}, {
+					name: 'Life',
+					info: ['Cheese Flies']
+				}]
+			});
+			break;
+		case "ert":
+			pageTemplate({
+				name: 'Ert',
+				file: 'ert.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['<a href="index.html?p=george" class="y">George (Binary System)</a>', '<a href="index.html?p=george" class="n">Perpahedron (Artificial)</a>']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Terrestrial']
+				}, {
+					name: 'Life',
+					info: ['Reegs', 'Polygons', 'And More']
+				}]
+			});
+			break;
+		case "george":
+			pageTemplate({
+				name: 'George',
+				file: 'george.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['<a href="index.html?p=ert" class="y">Ert (Binary System)</a>', 'Damocles']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Terrestrial']
+				}, {
+					name: 'Life',
+					info: ['Cows', 'Ozark', 'Plushrooms', 'And More']
+				}]
+			});
+			break;
+		case "dough":
+			pageTemplate({
+				name: 'Dough',
+				file: 'dough.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['Dot', 'Tod', 'Otd']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Torus Terrestrial']
+				}, {
+					name: 'Life',
+					info: ['Bacteria']
+				}]
+			});
+			break;
+		case "mac":
+			pageTemplate({
+				name: 'Mac',
+				file: 'mac.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['<a href="index.html?p=wop" class="y">World of Pain</a>']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Gas Giant']
+				}, {
+					name: 'Life',
+					info: ['Confirmed but Unknown']
+				}]
+			});
+			break;
+		case "wop":
+			pageTemplate({
+				name: 'World of Pain',
+				file: 'wop.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['None']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Terrestrial Object']
+				}, {
+					name: 'Life',
+					info: ['None']
+				}]
+			});
+			break;
+		case "aeiou":
+			pageTemplate({
+				name: 'Aeiou',
+				file: 'aeiou.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['Cdfqrszt']
+				}, {
+					name: 'Name Origin',
+					info: ['Named by the Alphabet Business Concern after concerns over The']
+				}, {
+					name: 'Classification',
+					info: ['Gas Giant']
+				}, {
+					name: 'Life',
+					info: ['None']
+				}]
+			});
+			break;
+		case "the":
+			pageTemplate({
+				name: 'The',
+				file: 'the.png',
+				caption: 'A mockup of what The could look like, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['Unknown']
+				}, {
+					name: 'Name Origin',
+					info: ['Named by an unknown organization after the Alphabet Business Concern attempted to name it Article.']
+				}, {
+					name: 'Classification',
+					info: ['Unknown']
+				}, {
+					name: 'Life',
+					info: ['Unknown']
+				}]
+			});
+			break;
+		case "cea":
+			pageTemplate({
+				name: 'Cea',
+				file: 'cea.png',
+				caption: 'Pictured in natural color, illustration made by Gramiatar.',
+				heading: 'Designations',
+				navbox: [{
+					name: 'Satellites',
+					info: ['Numerous Mines (52 confirmed)']
+				}, {
+					name: 'Name Origin',
+					info: ['Unknown']
+				}, {
+					name: 'Classification',
+					info: ['Ocean']
+				}, {
+					name: 'Life',
+					info: ['Unknown']
+				}]
+			});
 			break;
 		default: 
+			pageTemplate({
+				name: 'Oops, a tasty 404!'
+			});
 			document.write(`
-				<h1 style="font-family: Ubuntu;">Oops, a tasty 404!</h1>
-				
-				<hr>
-
 				<p>This page doesn't seem to exist, sadly... Maybe it was destroyed by World of Pain...</p>
 
 				</br>
@@ -174,3 +330,8 @@ function generatePage(page) {
 			`);
 	}
 }
+
+let page = new URLSearchParams(window.location.search);
+page = page.get('p')
+if (page == null) page = "main";
+generatePage(page);
