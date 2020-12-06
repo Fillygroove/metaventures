e890e50807ae5aac10a852f00f0ccff71788e1d2
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2020-12-05 21:05:28 -0500

    Update pagesetup.js

--- wiki/scripts/pagesetup.js
+++ wiki/scripts/pagesetup.js
@@ -394,30 +394,45 @@ function generatePage(page) {
 					info: ['Reegs', 'Polygons', 'And More']
 				}],
 				introText: [
-					'Ert is the fourth planet in the <a href="index.html?p=hotsun" class="y">Hot Sun</a> system, sharing this position with <a href="index.html?p=george" class="y">George</a>, after <a href="index.html?p=pilf" class="y">Pilf</a> and before <a href="index.html?p=dough" class="y">Dough</a>. Ert is notable, being one of the only two planets in a binary system, sharing this feature with George. Ert is also the planet with the most lifeforms, and also the most inteligent lifeforms. Ert is the planet that is the most advanced technology, including televisions, giant tungsten cubes, and the <a href="index.html?p=perpahedron" class="n">Perpahedron</a>. Ert is a seventieth of the size of the Hot Sun.',
-					'Ert\'s surface consists of silicon-based rocks with a multitude of different materials, organic and metallic. It is theorized that Ert has a core of molten rock. Ert\'s surface also contains water.',
+					'Ert is the fourth planet in the <a href="index.html?p=hotsun" class="y">Hot Sun</a> system, sharing this position with <a href="index.html?p=george" class="y">George</a>, after <a href="index.html?p=pilf" class="y">Pilf</a> and before <a href="index.html?p=dough" class="y">Dough</a>. Ert is notable, being one of the only two planets in a binary system, sharing this feature with George. Ert is also the planet with the second most diversity of life, behind George, and also has some of the most "intelligent" inhabitants. Ert’s sentient life boasts technology such as televisions, giant tungsten cubes, and the Perpahedron, but it is by no means able to take advantage of the planets around it, despite lifeforms being able to live on other planets. Ert is about one seventieth of the size of the Hot Sun.',
 					'The term for describing something related to Ert is Ertian.'
 				],
 				categories: [{
 					name: 'Characteristics',
 					info: [
-						'Ert is a Terrestrial planet that is made up of a silicon-based material, along with a large amount of water. The water is able to contain life. The surface itself contains a numerous amount of life, such as Reegs and Polygons. '
+						'Ert is a Terrestrial planet that is made up of a silicon-based material, covered in oceans of water. A day on Ert lasts exactly 1 Ert day, and a year on Ert lasts exactly Ert 1 year. How useful is that? Very useful, according to scientists, probably smoking bread. Yes, smoking bread is possible on Ert.','<h3>Appearance</h3>',
+						'Ert is a terrestrial planet covered in vast oceans of water. It features several continents and a breathable atmosphere. It is roughly spherical in shape. Colors consist of various blues and greens, a signature look akin to its smaller twin George. Some whites and browns are present as well. Ert also has an atmosphere visible as a bluish glow tightly wrapped around its body visible when illuminated. Ert’s tilt is around 0.5451523 radians (31.234928 degrees). Despite MANY claims that Ert’s shape should be a torus, it is in fact a sphere (but a slightly squashed).',
+						'<h3>Chemistry</h3>',
+						'Ert contains a breathable atmosphere, likely containing oxygen and nitrogen, along with other trace elements. The surface on Ert is silicon-based, and Ert’s oceans are made mostly of water and some concentration of salt. Ert also features a water cycle, which involves water evaporating, having a disco party in the clouds, and then falling asleep, heading back towards the ground. Obviously that isn’t exactly how it goes but it’s close enough. Most of the chemical processes on Ert is a direct consequence of being absolutely infested with life, of which dictates a lot of why it is so interesting to begin with. Life on there is so interesting that a slice of live TV show can last 30 seasons just from the main character eating cake.',
+						'<h3>Internal Structure</h3>',
+						'Ert has signs of volcanism on its surface, suggesting an active interior and a molten core. Its surface is differentiated by many large land masses, which is likely an indicator of plate tectonics. Ert is the fourth densest planet in the Hot Sun system, preceding George, Grēne, and The.',
+						'<h3>Magnetic Field</h3>',
+						'Ert has a robust magnetic field caused by interactions with George. It is strong enough to deflect solar radiation and thus protecting the two worlds. Despite its strength it is nowhere near as strong as the fields on Mac, Aeiou, and The.',
+						'<h3>Climate</h3>',
+						'Ert’s temperature is cooler than George by a few degrees. Its tilt causes seasonal shifts throughout its orbit around the Hot Sun. The highest recorded temperature on Ert 316.15 degrees kelvin, and the coldest is 177.15 degrees kelvin. Ert’s relatively mild temperature is beneficial to its inhabitants.'
 					]
 				}, {
 					name: 'Satellites',
 					info: [
 						'<h3>George</h3>',
-						'',
+						'<img src="images/george.png" alt="george.png" width="100" height="100" class="pixel" style="float: right; background-color: #555555;"></img>',
+						'George is Ert’s twin. George and Ert rotate around each other, and are tidally locked with one another. George has more land than Ert, and harbors more life. It also harbors Ozark, the entity that inspired the Dint Can’s mascot.',
+						'<img src="images/perpahedron.png" alt="perpahedron.png" width="300" height="300" class="pixel" style="float: right; background-color: #555555;"></img>',
 						'<h3>Perpahedron</h3>',
-						''
+						'The Perpahedron is an artificial satellite, constructed by Daktor and a group of scientists. The purpose of The Perpahedron is unknown, although suggestions for its purpose include being a gigantic multitool, a space mansion, and an overcomplicated mayonnaise jar. The Perpahedron has a large amount of tools, such as thrusters, speakers, and a giant tungsten cube attached by a nylon string.'
 					]
 				}, {
 					name: 'Life',
 					info: [
+						'Ert consists of an uncountable variety of life in many different phylums, but we counted those that only matter. Of course there are much more than listed below, but we just don’t feel like explaining what’s already obvious to most visitors. Do it yourself if you’re so worried about the state of every single mushroom. To summarize - plants, animals, fungi, and giant protists are present all over.',
 						'<h3>Reegs</h3>',
-						'Reegs are creatures that are most commonly blue. They feature two legs, two eyes, and sometimes a nose which can be used for attack, defense, as utilities, and more. Examples of notable Reegs are <a href="index.html?p=aero" class="y">Aero</a>, <a href="index.html?p=lakys" class="y">Lakys</a>, <a href="index.html?p=archie" class="y">Archie</a>, <a href="index.html?p=aposteel" class="y">Aposteel</a>, and <a href="index.html?p=royale" class="y">Royale</a>',
+						'Reegs are creatures that are most commonly blue. They feature two legs, two eyes, and sometimes a nose which can be used for attack, defense, as utilities, and more. Examples of notable Reegs are <a href="index.html?p=aero" class="n">Aero</a>, <a href="index.html?p=lakys" class="n">Lakys</a>, <a href="index.html?p=archie" class="n">Archie</a>, <a href="index.html?p=aposteel" class="n">Aposteel</a>, and <a href="index.html?p=royale" class="n">Royale</a>. Reegs are a prominant sentient species capable of many (and not so bright) things. They share their dominance to the Polygons.',
 						'<h3>Polygons</h3>',
-						'Polygons are simple geometric creatures infused with <a href="index.html?p=nagic" class="n">Nagic</a>. They feature stick legs and huge eyes. An example of a notable Polygon is <a href="index.html?p=pon" class="n">Pon</a>.'
+						'Polygons are simple geometric creatures infused with <a href="index.html?p=nagic" class="n">Nagic</a>. They feature stick legs and large eyes. They come in many colors, shapes, and sizes. Their society is built on the type of regular polygon, such as a square or octagon. Examples of notable polygons are <a href="index.html?p=pon" class="n">Pon</a>, <a href="index.html?p=larry-pv" class="n">Larry</a>, <a href="index.html?p=beth" class="n">Beth</a>, and <a href="index.html?p=vinny" class="n">Vinny</a> (although in technicality Pon, Vinny, and Beth are artificial creations). Like reegs, Polygons are prominent and capable of doing many (and much more brighter) things.',
+						'<h3>Spirons</h3>',
+						'Spirons are subterranean spider-like inhabitants featuring coiled appendages and four eyes. They inhabit most of the interior of Ert’s crust, appearing only very recently in history. Examples of notable spirons are <a href="index.html?p=coylstone" class="n">Coyl Stone</a>, Boomerang, The Great Helix, and Wallflower.',
+						'<h3>Florodites</h3>',
+						'Florodites are plant-like hybrids that live in small detached islands living together in tribes. They have the unique property of photosynthesising. Other than that they are unfortunately shoved aside by bigger factions.'
 					]
 				}]
 			});
