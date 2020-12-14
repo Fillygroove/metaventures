53a72fdecdda58a116efa4e144956a16dc6ca604
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2020-12-13 21:40:54 -0500

    Update comicSetup.js

--- scripts/comicSetup.js
+++ scripts/comicSetup.js
@@ -261,29 +261,29 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let slides = document.getElementsByClassName("slides");
+	let slides = document.getElementsByClassName("slides");
 
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
 
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
 
-    slides[slideIndex - 1].style.display = "block";
+	slides[slideIndex - 1].style.display = "block";
 }
 
 function verify(slide) {
-    let slides = document.getElementsByClassName("slides");
+	let slides = document.getElementsByClassName("slides");
 
 	if (comic == '9' && slide == '999') window.location.href = 'index.html?c=nine';
 
-    if (!isNaN(slide)) {
+	if (!isNaN(slide)) {
 		if (slide < slides.length + 1 && slide > 0) {
 			currentSlide(slide);
 		}
