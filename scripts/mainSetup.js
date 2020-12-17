10f0f254d6bd8fedc05a0cd29c60bc539d1bb378
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2020-12-16 21:24:32 -0500

    Update mainSetup.js

--- scripts/mainSetup.js
+++ scripts/mainSetup.js
@@ -7,7 +7,7 @@ mainH1.innerHTML = "Metaventures!";
 let lineBreak = document.createElement('hr');
 
 let mainParagraph = document.createElement('p');
-mainParagraph.innerHTML = 'Welcome to the Metaventures website! Metaventures is the name for a collective of comics and ideas, with a story art throughout througout space and time.';
+mainParagraph.innerHTML = 'Welcome to the Metaventures website! This place is still work in progress, so don\'t expect much. Check out the discord for more information!';
 
 let mainBreak1 = document.createElement('br');
 let mainBreak2 = document.createElement('br');
