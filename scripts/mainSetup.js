a2f33a9da0efd7b014ec759f7570a512285ea100
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2020-12-19 14:14:47 -0500

    Update mainSetup.js

--- scripts/mainSetup.js
+++ scripts/mainSetup.js
@@ -9,7 +9,4 @@ let lineBreak = document.createElement('hr');
 let mainParagraph = document.createElement('p');
 mainParagraph.innerHTML = 'Welcome to the Metaventures website! This place is still work in progress, so don\'t expect much. Check out the discord for more information!';
 
-let mainBreak1 = document.createElement('br');
-let mainBreak2 = document.createElement('br');
-
-mainAVThin.append(mainH1, lineBreak, mainParagraph, mainBreak1, mainBreak2);
+mainAVThin.append(mainH1, lineBreak, mainParagraph);
