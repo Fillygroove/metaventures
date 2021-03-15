143b57924693e21acf183dee125cb7b49608fa33
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2021-03-15 19:33:58 -0400

    Readability!

--- wiki/pages/nav_planet.js
+++ wiki/pages/nav_planet.js
@@ -1,24 +1,74 @@
+let imageSize = {
+	large: 30,
+	norm: 25,
+	small: 20
+};
+
 endnavs.push({
 	heading: 'The Bisolar System',
 	categories: [{
-		category: '<a href="index.html?p=hotsun"><img src="images/hot_sun.png" width="30" height="30">Ommadawn</a> System',
+		category: `<a href="index.html?p=hotsun"><img src="images/hot_sun.png" height="${imageSize.large}">Ommadawn</a> System`,
 		info: [{
 			name: 'Inner System',
 			info: [
-				'<a href="index.html?p=grene"><img src="images/grene.png" width="25" height="25"></img>Grene</a>',
-				'<a href="index.html?p=doug"><img src="images/doug.png" width="25" height="25">Doug</a> (<small><img src="images/h.png" width="20" height="20">H</small>)',
-				'<a href="index.html?p=pilf"><img src="images/pilf_ringless.png" width="25" height="25">Pilf</a>',
-				'<a href="index.html?p=ert"><img src="images/ert.png" width="20" height="20">Ert</a>  (<small><a href="index.html?p=perpahedron">Perpahedron</a></small>)',
-				'<a href="index.html?p=george"><img src="images/george.png" width="20" height="20">Georg</a><a href="images/george_real.png" class="y">e</a> (<small><img src="images/damocles.png" width="20" height="20">Damocles</small>)',
-				'<a href="index.html?p=baeg"><img src="images/baeg.png" width="25" height="20">Baeg</a>'
+				// Grene
+				`<a href="index.html?p=grene">
+					<img src="images/grene.png" height="${imageSize.norm}"></img>
+					Grene
+				</a>`,
+				// Doug
+				`<a href="index.html?p=doug">
+					<img src="images/doug.png" height="${imageSize.norm}">
+					Doug
+				</a>
+				(<small><img src="images/h.png" height="${imageSize.small}">H</small>)`,
+				// Pilf
+				`<a href="index.html?p=pilf">
+					<img src="images/pilf_ringless.png" height="${imageSize.norm}">
+					Pilf
+				</a>`,
+				// Ert
+				`<a href="index.html?p=ert">
+					<img src="images/ert.png" height="${imageSize.norm}">
+					Ert
+				</a>
+				(<small><a href="index.html?p=perpahedron">Perpahedron</a></small>)`,
+				// George
+				`<a href="index.html?p=george">
+					<img src="images/george.png" height="${imageSize.norm}">
+				Georg</a><a href="images/george_real.png" class="y">e</a>
+				(<small><img src="images/damocles.png" height="${imageSize.small}">Damocles</small>)`,
+				// Baeg
+				`<a href="index.html?p=baeg">
+					<img src="images/baeg.png" height="${imageSize.norm}">
+					Baeg
+				</a>`
 			]
 		}, {
 			name: 'Outer System',
 			info: [
-				'<a href="index.html?p=mac"><img src="images/mac.png" width="35" height="25">Mac</a> (<small><a href="index.html?p=wop"><img src="images/wop.png" width="23" height="20">World of Pain</a></small>)',
-				'<a href="index.html?p=hazel">Hazel</a> (<small>Curfew &bull; Purpul &bull; Cea</small>)',
-				'<a href="index.html?p=aeiou"><img src="images/aeiou.png" width="27" height="25">Aeiou</a> (<small>Cdfqrszt &bull; Heideph</small>)',
-				'<a href="index.html?p=the"><img src="images/the.png" width="25" height="25">The</a>'
+				// Mac
+				`<a href="index.html?p=mac">
+					<img src="images/mac.png" height="25">
+					Mac
+				</a>
+				(<small><a href="index.html?p=wop"><img src="images/wop.png" height="20">World of Pain</a></small>)`,
+				// Hazel
+				`<a href="index.html?p=hazel">
+					<img src="images/hazel.png" height="${imageSize.norm}">
+					Hazel
+				</a>`,
+				// Aeiou
+				`<a href="index.html?p=aeiou">
+					<img src="images/aeiou.png" height="${imageSize.norm}">
+					Aeiou
+				</a>
+				(<small>Cdfqrszt &bull; Heideph</small>)`,
+				// The
+				`<a href="index.html?p=the">
+					<img src="images/the.png" height="${imageSize.norm}">
+					The
+				</a>`
 			]
 		}]
 	}, {
@@ -26,7 +76,10 @@ endnavs.push({
 		info: [{
 			name: 'Inner System',
 			info: [
-				'<a href="index.html?p=coldsun"><img src="images/cold_sun.png" width="25" height="25">Cold Sun</a>'
+				`<a href="index.html?p=coldsun">
+					<img src="images/cold_sun.png" height="${imageSize.norm}">
+					Cold Sun
+				</a>`
 			]
 		}, {
 			name: 'Outer System',
