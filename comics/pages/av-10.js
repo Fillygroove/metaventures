726d4dc1fcc177cd01c736a2f71c0f8bfedd85d2
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2021-04-29 20:21:27 -0400

    Update av-10.js

--- comics/pages/av-10.js
+++ comics/pages/av-10.js
@@ -2,6 +2,7 @@ let comicInfo = {
 	title: 'AV-10: Flat Line',
 	desc: '<a href="../wiki/index.html?p=aero">Aero</a> goes to the hospital after an incident and finds out that it isnt as it seems. porkchops included.',
 	folder: 'av/av-10/',
+	background: 'bg-av.png',
 	rewindTo: 'av-choose'
 };
 
