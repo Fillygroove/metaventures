70320d19de536ce533924b61f5a5f093553bd4a4
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2021-06-16 23:15:58 -0400

    Update doug.js

--- wiki/pages/doug.js
+++ wiki/pages/doug.js
@@ -21,7 +21,7 @@ let pageData = {
 				info: ['Terrestrial']
 			}, {
 				name: 'Life',
-				info: ['Worms (Unconfirmed)']
+				info: ['Worms']
 			}]
 		}, {
 			heading: 'Theme',
@@ -38,103 +38,119 @@ let pageData = {
 			quote: 'I\'m shocked that nobody really looked at this planet before, it\'s been left out of discussions for a long time. I haven\'t really found anything, and it\'s NOT because I HAVEN\'T been looking.',
 			author: '<a href="index.html?p=lakys">Dr. R. Lakys</a>'
 		},
-		'Doug is the second planet in the Ommadawn system, after Grēne and before Pilf. Doug has been referred to as the most disliked planet. Reasons include the periodic DIE bursts that cut off satellite communication or that it serves no purpose in figuring out new physics. Doug resembles a brown-yellow ball of dirt, or to some degree, a burnt chocolate chip cookie.',
+		'Doug is the second planet in the Ommadawn system, after <a href="index.html?p=grene">Grēne</a> and before <a href="index.html?p=pilf">Pilf</a>. Doug has been referred to as the most disliked planet among scientists. Reasons include the barren planet’s landscapes or that it serves no purpose in figuring out new physics. Doug resembles a brown-yellow ball of dirt, or to some degree, a burnt chocolate chip cookie.',
+
 		'<h3>Interesting Facts</h3>',
 		[
-			'Once a year, Doug emits a burst of energy known as a DIE Burst.',
 			'Doug has a moon that somewhat resembles itself and Pilf.',
-			'Doug has a near-perfect circular orbit',
-			'Doug may have worms inside.',
-			'The term for describing something related to Doug is Forgettable.',
+			'Doug has a near-perfect circular orbit.',
+			'Doug has worms inside.',
+			'Doug is the culprit of a strange burst of energy known as a DIE Burst.',
+			'The term for describing something related to Doug is Dougian, or Forgettable.',
 			'That\'s it. That\'s all you need to know.'
-		]
+		],
 	],
 	categories: [{
 		name: 'Coverage',
 		info: [
 			'Doug will appear in AV and PV comics in the future.',
+
 			'<h3>Aeroventures</h3>',
 			'Not much here at the moment.',
+
 			'<h3>Polyventures</h3>',
 			'Not much here at the moment.',
+
 			'<h3>ATG</h3>',
 			'Not much here at the moment.',
+
 			'<h3>Janet Odyssey</h3>',
 			'In the game concept, Doug would be a somewhat early level.',
-			'Doug has a song for the Album. It is described as jazzy.'
+			'Doug has a song currently in the OST, which was inspired by types of jazz.'
 		]
 	}, {
 		name: 'Planet Info',
 		info: [
 			{
-				quote: "When a planet becomes so boring that all other worlds feel so different, you get Doug.",
-				author: 'Anonymous Author'
+				quote: 'When a planet becomes so boring that all other worlds feel so different, you get Doug',
+				author: 'Kudos Palinski (Director of Doug Sciences or "Dee Scee")'
 			},
 			'Doug is a terrestrial planet, meaning that it is possible to not stand on its surface.',
+			
 			'<h3>Compared to Other Planets...</h3>',
 			[
 				'Doug is the second smallest terrestrial planet.',
 				'By size it is the tenth largest planet.',
 				'By mass it is the ninth largest planet.',
 				'By density, Doug is in fifth place.',
-				'By distance to Ommadawn, Doug is second.',
+				'By distance to <a href="index.html?p=hotsun">Ommadawn</a>, Doug is second.',
 				'By difficulty to visit, Doug is third.',
 				'By habitability, Doug is the second worst excluding the giant planets.',
 				'By difficulty to remember, Doug is first.',
-				'By how ugly it is, Doug is also first.'
+				'By how rugged it is, Doug is also first.'
 			],
-			'Unsurprisingly, Doug\'s placement is average and unremarkable. Being the best at being forgotten!',
-			'<h3>Orbit And Rotation</h3>',
-			'Doug resides in a balmy distance from Ommadawn, just far enough to not be melted by the heat, <i>unfortunately</i>. It is closer to the sun compared to Mercury.',
-			'Doug\'s orbit is nearly a perfect circle, which is surprising considering that it would often dragged around by the other nearby planets, most noteworthy is Grēne and the Ert and George binary. The overall stability of its placement may be a lucky coincidence.',
-			'Doug\'s rotation is rather slow, possibly due to the way the planet was created, likely from a "cataclysmic" event. The rotation is so slow that only around one fifth of a day passes when compared to a year on Doug. If you were born on Doug, you\'d be one year old before you\'d be one day old.',
+			'Unsurprisingly, Doug\'s placement is average and unremarkable.',
+			
+			'<h3>Orbit and Rotation</h3>',
+			'Doug resides in a balmy distance from Ommadawn, just far enough to not be melted by the heat, unfortunately. It is closer to the sun compared to real-life Mercury.',
+			'Doug\'s orbit is nearly a perfect circle, which is surprising considering that it would be dragged around by the other nearby planets, most noteworthy is Grēne and the Ert and George binary. The overall stability of its placement may be a lucky coincidence.',			
+			'Doug\'s rotation is rather slow, possibly due to the way the planet was created, likely from a "cataclysmic" event. The rotation is so slow that only around one fifth of a day passes when compared to a year on Doug. If you were born on Doug, your first birthday will happen before the day ends.',
+			'Doug\'s axial tilt is perpendicular to its orbit, resting at a nice 0 degrees. This means that Doug experiences no seasons.',
 			'One of the few interesting side-effects of Doug\'s slow rotation is that if you avoid all warnings and stand on its surface, Ommadawn will appear to freeze in the sky at certain points of the day.',
+
 			'<h3>Geography</h3>',
 			'Doug\'s most notable features are its patches of yellows, tans, and browns. It features a few massive craters that scar the surface with dark basins. Its surface is an entirely solid landscape full of jagged mountains, ridges, plains, and craters from ancient collisions.',
-			'A group of three craters (pictured at the middle-left of the image of Doug used in the infobox), which, from left to right, are named Mer, Z, and Bau. These craters lack any evidence of impact, suggesting that the formation of these craters come from inside of the planet.',
+			'A group of three craters (pictured at the middle-left of the image of Doug used in the infobox), which, from left to right, were codenamed Mer, Z, and Bau. These craters lack any evidence of impact, suggesting that the formation of these craters come from inside of the planet.',
 			'The brighter patches seem to be older than the darker patches, hinting on Doug\'s more active past.',
+			
 			'<h3>Geology</h3>',
 			'Given the clues on its surface, Doug may have had an active interior. It is likely that it has a crust and mantle, with a liquid core of metals. It may be porus inside.',
-			'Once every Ert year, Doug emits a large burst of energy, referred to as a DIE Burst (Doug Internet EMP Burst). This burst of energy cuts out all forms of communication around Ert, which gives people a good reason to hate it, as if they didn\'t have a good enough reason in the past.',
-			'The source of these bursts are unknown, and most current models suggest that they shouldn\'t be possible. They appear to be strongest within the large craters, and the trajectory of these bursts face only towards the Ert and George binary, even when Doug is at opposition.',
+			
 			'<h3>Magnetic Field</h3>',
-			'Doug has a weak magnetic field, almost capable of deflecting charged particles from Ommadawn. During a DIE Burst, the strength of this field is increased by over a million times. How this field came to be is beyond anyone\'s knowledge, likely due to nobody caring enough.',
+			'Doug normally has a weak magnetic field, almost capable of deflecting charged particles from Ommadawn. On rare occasions, Doug emits a burst of ionizing radiation of various strengths, referred to as "DIE Bursts" (Doug Internet EMP Burst). This burst is capable of temporarily disrupting satellite communication, which gets on the nerves of NA telecommunicators.',
+			'The cause of these bursts are largely understudied. They appear to be strongest within the large craters, and the trajectory of these bursts usually face towards the Ert and George binary, sometimes even when Doug is at opposition.',
+			
 			'<h3>Atmosphere and Climate</h3>',
-			'Doug has an exosphere, which does nothing and practically does not exist. Scientists theorize that the exosphere comes from Doug capturing bits of Grēne\'s ejected atmosphere. This may be the explanation of Doug\'s brown surface, being the result of gradual interactions from the trail.',
-			'Surface temperatures of Doug would be blistering hot or freezing cold. Since there is no medium to have heat transfer, it would be equivalent to being in a vacuum. Climates vary little, only determined by how much light a certain part of the planet may receive. Doug\'s slow rotation would result in long days and nights, fluctuating between both extremes.',
-			'Doug is considered the second-hottest planet overall, although it is still far behind Grēne by a longshot.',
+			'Doug has an exosphere, which does nothing and practically does not exist. Scientists theorize that the exosphere comes from Doug capturing bits of Grēne\'s ejected atmosphere. This may be the explanation of Doug\'s brown surface, being the result of gradual interactions from the trail.',			
+			'Surface temperatures of Doug would be blistering hot or freezing cold. Since there is no medium to have heat transfer, it would be equivalent to being in a vacuum. Climates vary little, only determined by how much light a certain part of the planet may receive. Doug\'s slow rotation would result in long days and nights, fluctuating between both extremes. Doug is considered the second-hottest planet overall, although it is still far behind Grēne by a longshot.',
+			
 			'<h3>Satellites</h3>',
-			'The only confirmed moon is Aech, which resembles Doug but also happens to be in the shape of a cube, similar to that of Pilf. Aech is tiny and orbits barely a Doug radius away from the surface.',
+			'The only confirmed moon is <a href="./index.html?p=aech">Aech</a>, which resembles Doug but also happens to be in the shape of a cube, similar to that of Pilf. Aech is tiny and orbits barely a Doug radius away from the surface.',
 			'Surrounding Doug is a dispersed asteroid field, ranging from a few meters to almost a kilometer in size. These rocks are believed to be fragments from the moon Aech as it approaches Doug and gets ripped apart by its gravity. This may form a ring around Doug.',
+			
 			'As Doug hasn\'t been investigated much by modern day scientists, it is unknown if more than one moon existed in the past.',
 			'<h3>Life</h3>',
-			'Doug\'s surface conditions are hazardous to most forms of life. Surface temperatures would burn or freeze anything that dares to traverse on its surface. Without a sufficient atmosphere or a proper form of radiation protection, most life would die.', 
-			'Despite the inhospitality on its surface, there may be a worm-like creature borrowing beneath the surface. While unconfirmed as of now, these "worms" would be the reason why there are deep holes in Doug. This theory is also applicable to Aech.',
-			'Most scientists would confidently say that there\'s nothing to see here, and would promptly tell you to defenestrate yourself.',
+			'Doug\'s surface conditions are hazardous to most forms of life. Surface temperatures would burn or freeze anything that dares to traverse on its surface. Without a sufficient atmosphere or a proper form of radiation protection, most life would die.',
+			'Despite the inhospitality on its surface, there are signs of worm-like creatures borrowing beneath the surface. While unconfirmed as of now, these "worms" would be the reason why there are deep holes in Doug. This theory is also applicable to Aech. This has caused many Ertlings to reference Doug in any worm related content.',
+			'Most scientists would confidently say that there\'s nothing interesting to see here, and would promptly tell you to defenestrate yourself, as you are making the room hot with your breath.',
+
 			'<h3>Symbol</h3>',
-			'Doug\'s symbol represents an upside-down question mark, which refers to the fact that every question about Doug is unceremoniously cast away in the downstairs trash can. There may have been a different symbol for it in the past.'
+			'Doug\'s symbol represents a humbling caricature of a face, to represent Doug\'s strong stature despite the many difficulties it goes through even in research. Many Ertlings look up to Doug and refer to Doug as "The Humble" as a representation of strength during times of hardship.'
 		]
 	}, {
 		name: 'Culture',
 		info: [
-			'Doug is universally regarded to be the least favored planet according to the general demographics of Ert cultures. It is widely hated due to the opinion of scientists, claiming there is little incentive for any form of science other than protection from the notorious DIE bursts. Doug is the least visited planet in the Hot Sun system.',
-			'In the Reegublic Zodiacal Calendar, the first month of the year is Doug. It has been tradition for those who were born during this time to be tossed into a dumpster. Regulations changed that rule to offer a bag of tricks to the recipient who was born in Doug annually.',
-			'<h3>The DeeCee</h3>',
-			'The routinely teased "Doug" generations formed a collective called the "Doug Club" or DeeCee to prove the notion of Doug\'s hate to be unjust. This group comprises many Doug enthusiasts willing to learn much of everything there is to be discovered from the planet. The DeeCee has managed to earn enough support from the vastly criticised Nyork Administration and Alphabet\'s Business Concern to send a probed mission to Doug.',
-			'The term "Forgettable" was an agreed replacement to the term "Dougian" in order to sway away the general public opinion to not be reminded of the word "Doug" in describing things related to Doug\'s parameters. Strangely for the DeeCee, saying "Forgettable" often is seen as a compliment and the speaker would receive praise in doing so.',
+			'Doug is regarded to be a moderately favored planet according to the general demographics of Ert cultures. It is widely hated to the opinion of scientists however, claiming there is little incentive for any form of science other than protection from the notorious DIE bursts. This opinion also stems from the researchers of Doug, who were unceremoniously given the most "average" planet out of the fantastical bunch. The researchers had a notably small attention span and started the trend of despising Doug for taking their summer away.',
+			'In the Reegublic Zodiacal Calendar, the second month of the year is Doug. It has been tradition for scientists to throw a "Doug Party" where they all attempt to humiliate the planet Doug for fun, whoever has the most complicated reasoning for Doug\'s inadequacy, will get boxes of chocolate chip cookies or "Doug" Cookies, referred to below.',
+			'Doug has had an impact on the public outlook on worms due to the signs of similar creatures in Doug\'s caverns. Doug is a common name for a pet worm, and a species of giant Ertworm is a "Douggian" Worm, referencing the massive size of Doug\'s anticipated worms. Doug Cookies are chocolate chip cookies with gummy worms poking out the sides.',
+
 			'<h3>Exploration</h3>',
-			'Exploring Doug is costly and difficult, requiring a lot of maneuvers to even approach it. There is danger of potential collisions with Doug\'s asteroid field and the DIE bursts, which at this distance would be lethal for any unprepared mission.',
-			'Doug only received two missions: The Forgotten Explorer, and the mission profile for Janet.',
-			'The Forgotten Explorer was a successful mission by the DeeCee, imaging and mapping most of Doug\'s surface. It was eventually damaged by a DIE Burst and all communication to it was lost.',
-			'There is little known about Janet and the mission to Doug.',
+			'Doug is the least visited planet in the Hot Sun system. Exploring Doug is costly and difficult, requiring a lot of maneuvers to even approach it. There is danger of potential collisions with Doug\'s asteroid field and the DIE bursts, which at this distance would be lethal for any unprepared mission.',
+			'In spite of the challenging parameters, Doug has received two missions: The Forgotten Explorer, and Odyssey.',
+			'The Forgotten Explorer was the first successful mission. It provided a wealth of data from all over the planet, with its biggest discoveries being the DIE bursts and the presence of worms. While it survived longer than its planned lifetime, It was eventually damaged by an unexpected DIE Burst and all communication to it was lost.',
+			'There is little known about Janet and the mission to Doug. However, in general to Janet\'s "Odyssey" mission, it has provided the NA with the current images and data on numerous planets.',
+			
+			'<h3>Dee Scee (Doug Sciences)</h3>',
+			'The Dee Scee is a pet name for the Doug Science Division of the Nyork Research Administration. The division includes people who take joy in figuring out Doug\'s mundane mysteries. In some ways, they see it as a humbling, tough little planet who is largely misunderstood. This exact sentence caused Dr. R Lakys to throw a tray of Doug Cookies into the trash and leave the division, as he had thought the Doug Sciences would help him on his outlandish plan to explode Doug with a giant space laser. Lakys was then seen in a trash bin outside the building sucking on his thumb and fired from the Nyork Administration a month afterwards due to other similar complaints which may or may not include giant space lasers. The Dee Scee contains such notable Assets as Kudos Palinski (Head), "FEON" (Forgotten Explorer Orbiter), and Janet Parhelion (Odyssey Lead).',
+
 			'<h3>Scientists\' Opinion</h3>',
-			'Physicists attribute Doug to being a useless asset in their quest of discovering and proving general relativity due to its circular orbit.',
+			'Stern Physicists attribute Doug to being a useless asset in their quest of discovering and proving general relativity due to its circular orbit.',
 			'Ground astronomers have a general distaste for Doug due to its dull non-reflective surface, which renders it mostly invisible when viewed in the evening skies of Ert.',
-			'A professional baker once said that Doug would be the worst dessert to serve to children.',
+			'The Dee Scee has an overwhelming positive opinion on Doug deeming it to be just as interesting as any other planet in the Bisolar System.',
 			'A quote which has been highly debated by critics yet accepted by scientists may be read below.',
-			{	
+			{
 				quote: 'Doug, are you KIDDING ME? Why can\'t this damn planet make VOLCANOES already! I want to see some action, not a dead dirtball of a planet! I don\'t even want to call it a planet, it\'s just a rock. It has absolutely NO value to my research.',
-				author: '<a href="index.html?p=lakys">Dr. R. Lakys</a>'
+				author: 'Dr. R. Lakys'
 			}
 		]
 	}, {
