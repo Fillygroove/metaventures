d8fdc2b77e3db05313ce2ae2a7b32f5f9db03f6a
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2021-06-29 12:48:58 -0400

    Removed pageName debug info

--- scripts/wikiSetup.js
+++ scripts/wikiSetup.js
@@ -2,8 +2,6 @@ let pageName = new URLSearchParams(window.location.search);
 pageName = pageName.get('p');
 if (pageName == null) pageName = "main";
 
-console.log(pageName);
-
 let warnings = [];
 let endnavs = [];
 	
